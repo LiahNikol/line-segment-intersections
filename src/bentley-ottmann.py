afde9b50682879c31931aa4c030a6f0c8b6a6e18
LiahNikol <38577516+LiahNikol@users.noreply.github.com>
2021-04-29 01:44:36 -0400

    Update bentley-ottmann.py
    Final implementation of main algorithm

--- src/bentley-ottmann.py
+++ src/bentley-ottmann.py
@@ -1,52 +1,63 @@
-import numpy as np
-
 # Class implements the Bentley Ottmann intersections for a set of segments algoriths
-from bst import BST
+from endpoint import Endpoint
+from segment import Segment
+from intersection import Intersection
+from helper import checkIntersect, isValidPos
 
 class BentleyOttmannAlgo:
-  def main(segments): # {([x_1, y_1], [x_2, y_2]), ...}
-    # initializing data structures
-    eq = BST() # event queue implemented as a binaryTree containing all endpoints sorted by increasing x and y
-    sl = # activeList sorted by increasing y of leftendpoints
-    output = []
+  # initializing data structures
+  eq = BST() # event queue implemented as a binaryTree containing all endpoints sorted by increasing x and y
+  sl = OrderedList()
+  output = []
+            
+  def main(segments): # [[(x_1, y_1), (x_2, y_2), ...]]
     for segment in segments:
       newSeg = Segment(segment[0], segment[1])
-      eq.add(newSeg.getLeftEndpoint)
-      eq.add(newSeg.getRightEndpoint)
+      eq.add(newSeg.getLeftEndpoint())
+      eq.add(newSeg.getRightEndpoint())
 
     # main algorithm
     while eq.size() > 0:
       event = eq.removeNext()
-      if isinstance(event, Endpoint) && event.isLeft():
+    
+      if isinstance(event, Endpoint) && event.isLeft(): # event is a left endpoint
         seg = event.getSegment()
-        segPosition = sweepLine.add(seg) # add this segment to the active list
-        belowIdx = segPosition - 1; # check for intersections against active segment
-        if belowIdx >= 0:
-          belowSeg = sweepLine.get(belowIdx)
-          intersection = intersects(seg, belowSeg)
-          if intersection != -1: eventQueue.add(intersection)
-          
-        aboveIdx = segPosition + 1
-        if aboveIdx < sweepLine.size():
-          aboveSeg = sweepLine.get(aboveIdx)
-          intersection = intersects(seg, aboveSeg)
-          if intersection != -1: eventQueue.add(intersection)
-      elif isinstance(currentEvent, Endpoint) && currentEvent.isRight():
-        seg = currentEvent.getSegment()
-        segPosition = sweepLine.remove(seg)
-        belowIdx = segPosition - 1
-        aboveIdx = segPosition
-        if belowIdx >= 0 && aboveIdx < sweepLine.size():
-          belowSeg = sweepLine.get(belowIdx)
-          aboveSeg = sweepLine.get(aboveIdx)
-          intersection = intersects(belowSeg, aboveSeg)
-          if intersection != -1: eventQueue.add(intersection) # make sure intersection isn't already in there though
-      elif isinstance(currentEvent, Intersection): # currentEvent is an intersection
-        output.add(currentEvent)
-        segAbove = currentEvent.getSegAbove()
-        segBelow = currentEvent.getSegBelow()
-        sweepline.swap(segAbove, segBelow)
-        segAboveAbove = sweepLine.above(segBelow)
-        segBelowBelow = sweepLine.below()
-        # add intersections to eq if they don't already exist
-  return output
+        segPos = sl.add(seg) # add this segment to the active list
+        aPos = segPos + 1
+        bPos = segPos - 1
+        if isValidPos(aPos, sl):
+          aCoords = checkIntersect(aPos, seg, sl)
+          if len(aCoords) > 0:
+            eq.insert(aCoords)
+        if isValidPos(bPos, sl): 
+          bIntersection = checkIntersect(bPos, seg, sl)
+          if len(bCoords) > 0:
+            eq.insert(bCoords)
+            
+      elif isinstance(event, Endpoint) && event.isRight(): # event is a right endpoint
+        seg = event.getSegment()
+        segPos = sl.remove(seg)
+        aPos = segPosition
+        bPos = segPosition - 1
+        if isValidPos(aPos, sl) and isValidPos(bPos, sl):
+          bSeg = sl.get(bPos)
+          coords = checkIntersect(aPos, bSeg, sl)
+          if len(coords) > 0 and not eq.contains(coords):
+            eq.insert(coords)
+                
+      elif isinstance(event, Intersection): # event is an intersection
+        output.append(event)
+        segAbove = event.getSegAbove()
+        segBelow = event.getSegBelow()
+        newPos1, newPos2 = sl.swap(segAbove, segBelow) # segAbove is now below segBelow and segBelow is now above segAbove...
+        aPos = segBelow + 1
+        bPos = segAbove - 1
+        if isValidPos(aPos, sl):
+          aCoords = checkIntersect(aPos, segBelow, sl)
+          if len(aCoords) > 0 and not eq.contains(aCoords):
+            eq.insert(aCoords)
+        if isValidPos(bPos, sl): 
+          bCoords = checkIntersect(bPos, segAbove, sl)
+          if len(bCoords) > 0 and not eq.contians(bCoords):
+            eq.insert(bCoords)
+    return output

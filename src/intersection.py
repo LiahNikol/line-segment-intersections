7da310899f10edd1e1fad83e41bbd188bea107d6
LiahNikol <38577516+LiahNikol@users.noreply.github.com>
2021-04-29 01:46:20 -0400

    Update intersection.py

--- src/intersection.py
+++ src/intersection.py
@@ -5,6 +5,9 @@ class Intersection:
     self.segAbove = segAbove # segment that was above segBelow when approaching intersection from the left
     self.segBelow = segBelow # segment that was below segAbove when approaching intersection from the left
   
+  def __len__(self):
+    return 2 # and intersection object should always contains a tuple of 2 numbers, (x, y), representing a point
+  
   def getSegAbove(self):
     return self.segAbove
     

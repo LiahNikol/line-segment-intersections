dd971469c01b27f55625bc9d9630e95a760c7a8b
LiahNikol <38577516+LiahNikol@users.noreply.github.com>
2021-05-05 17:56:39 -0400

    Implemented new functions
    Implemented initialize_event_queue and add_each. Simplified containsEvent to use built-in list functionality but may need to be changed later.

--- src/PriorityQueue.py
+++ src/PriorityQueue.py
@@ -2,6 +2,7 @@
 # Utilizes heapq functions to maintain O(logn) complexity
 from heapq import heapify, heappush, heappop
 from Intersection import Intersection
+from Segment import Segment
 
 class PriorityQueue:
     def __init__(self):
@@ -13,9 +14,6 @@ class PriorityQueue:
     
     def __str__(self):
         return str(list(self.queue))
-        
-    def add(self, event):
-        heappush(self.queue, event)
 
     # This is a convenience method for adding multiple intersections to the event queue
     # (some operations will add multiple intersections, some will add none).
@@ -26,22 +24,29 @@ class PriorityQueue:
     # events = [i1]
     # events = [i1, i2]
     def add_each(self, events):
-        # TODO
-        return None
+        if events is None:
+            return
+        
+        for event in events:
+            if not self.containsEvent(event):
+                 heappush(self.queue, event)
+        
+        return
         
     def remove_min(self):
         return heappop(self.queue) # heappop removes the "smallest" element in the heap
         
     def containsEvent(self, event):
-        for element in self.queue:
-            if element == event:
-                return True
-        return False
+        return event in self.queue
         
     # This function should take all segments as a list like 
     # [((x11, y11), (x12, y12)), ((x21, y21), (x22, y22)), ...]
     # Then, it will convert these points into Segment objects and 
     # add the endpoints to the given event queue
     def initialize_event_queue(self, segments):
-        # TODO
-        return None
+        for segment in segments:
+            seg = Segment(segment[0], segment[1])
+            endpoints = seg.getEndpoints()
+            self.add_each(endpoints)
+        return
+            
